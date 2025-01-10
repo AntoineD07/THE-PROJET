2bf124245c7c16af00346183bc6bfa5529235271
AntoineD07 <antoinedelarue07@gmail.com>
2025-01-10 15:09:32 +0100

    dd

--- assets/js/panier.js
+++ assets/js/panier.js
@@ -7,8 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.quantite input[type="number"]').forEach(input => {
         input.addEventListener('change', function() {
             checkQuantity(this);
+            updateTotal();
         });
     });
+    updateTotal();
 });
 
 // fonction regarde si input == O si 0 suprime produit
@@ -16,14 +18,19 @@ function checkQuantity(input) {
     if (input.value == 0){
         const produit = input.closest('.produit');
         produit.remove()
+        updateTotal();
     }
 }
+//fonction qui calcule et met a jours le total
 
+function updateTotal() {
+    let total = 0;
+    document.querySelectorAll('.produit').forEach(produit =>{
+        const prix = parseFloat(produit.querySelector('.prix p').textContent.replace('€', ''));
+        const quantite = parseFloat(produit.querySelector('.quantite input[type="number"]').value);
+        total += prix *quantite;
+    });
+    document.querySelector('.total p:last-child').textContent = total + '€';
+}
+updateTotal();
 
-//je veux recuperer le prix et la quantiter et afficher le total
-const prix = document.querySelector('.prix p');
-const quantite = document.querySelector('.quantite input[type="number"]');
-
-quantite.addEventListener('input', () => {
-    console.log(`Quantité actuelle : ${quantite.value}`);
-});
